0b51713100de5cd1ccc9887beb2ce2fa20a6c9b6
eric bridgeford <ebridge2@jhu.edu>
2017-11-02 12:59:33 -0400

    updated to add interactive vignette demo

--- R/run_demo.R
+++ R/run_demo.R
@@ -1,17 +1,9 @@
-#' A simple demo function to illustrate MGC on linear and quadratic relationships at size 20.
+#' A simple demo function to illustrate MGC on test data.
 #'
 #' @export
-mgc.run_demo <- function(){
-  n=20;
-  a=rnorm(n, mean = 0, sd = 1)
-  b=3*a; # linear relationship
-  rep=1000;
-  mgc1=mgc.test(a,b,rep) # mgc test with p-value 0 and optimal scale always at the maximal (400)
-  cat('Under Linear Dependency of 20 observations, MGC Statistic, p-value, and optimal scales (matrix single index) are: ', mgc1$statMGC, ',', mgc1$pMGC,',',mgc1$optimalScale,'\n')
-
-  b=a^2; # quadratic relationship
-  mgc2=mgc.test(a,b,rep) #mgc test with p-value almost 0, and local optimal scale
-  cat('Under Quadratic Dependency of 20 observations, MGC Statistic, p-value, and optimal scales (matrix single index) are: ', floor(mgc2$statMGC*100)/100, ',', mgc2$pMGC,',',mgc2$optimalScale,'\n')
+mgc.run_demo <- function(x, y){
+  set.seed(12345)
+  mgc.sample(x=x, y=y)
 }
 #' A simple demo function to illustrate Discriminability on 2 realizations of 2 random variables.
 #'
