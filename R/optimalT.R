0b454c5a69b32d8a8325c89aa6b5e606dafd0ae3
youjin1207 <ylee160@jhu.edu>
2017-11-10 11:52:34 -0500

    mnt code

--- R/optimalT.R
+++ R/optimalT.R
@@ -13,21 +13,33 @@
 
 
 print.stat.optimal = function(stat.list, default.t){
-
+  
   null.stat = c(); null.t = c()
   
   if(max(order(-stat.list[[1]])[1:3])-min(order(-stat.list[[1]])[1:3]) == 2){
     alt.stat = stat.list[[1]][mean(order(-stat.list[[1]])[1:3])]
-    alt.t = mean(order(-stat.list[[1]])[1:3])
+    alt.t = mean(order(-stat.list[[1]])[1:3]) 
+  }else if(sum(order(-stat.list[[1]])[1:2] %in% c(1:2))==2){
+    alt.stat = stat.list[[1]][1]
+    alt.t = 1 
+  }else if(sum(order(-stat.list[[1]])[1:2] %in% c(length(stat.list[[1]]), (length(stat.list[[1]])-1)))==2){
+    alt.stat = stat.list[[1]][length(stat.list[[1]])]
+    alt.t = length(stat.list[[1]]) 
   }else{
     alt.stat = stat.list[[1]][default.t]
-    alt.t = default.t
+    alt.t =  default.t
   }
   
   for(j in 1:nrow(stat.list[[2]])){
     if(max(order(- stat.list[[2]][j,])[1:3])-min(order(-stat.list[[2]][j,])[1:3]) == 2){
       null.stat[j] = stat.list[[2]][j,mean(order(-stat.list[[2]][j,])[1:3])]
-      null.t[j] = mean(order(-stat.list[[2]][j,])[1:3])
+      null.t[j] = mean(order(-stat.list[[2]][j,])[1:3]) 
+    }else if(sum(order(-stat.list[[2]][j,])[1:2] %in% c(1:2))==2){
+      null.stat[j] = stat.list[[2]][j,1]
+      null.t[j] = 1 
+    }else if(sum(order(-stat.list[[2]][j,])[1:2] %in% c(length(stat.list[[2]][j,]), (length(stat.list[[2]][j,])-1)) ) == 2){
+      null.stat[j] = stat.list[[2]][j,length(length(stat.list[[2]][j,]))]
+      null.t[j] = length(length(stat.list[[2]][j,])) 
     }else{
       null.stat[j] = stat.list[[2]][j,default.t]
       null.t[j] = default.t
